d8956682b23a33a85d5218831b75455111ef0b39
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-02-06 11:43:43 +0000

    :clown_face: Added Error handlers for products

--- src/main/java/com/eyo/productservice/command/api/events/ProductEventHandler.java
+++ src/main/java/com/eyo/productservice/command/api/events/ProductEventHandler.java
@@ -2,11 +2,15 @@ package com.eyo.productservice.command.api.events;
 
 import com.eyo.productservice.command.api.data.Product;
 import com.eyo.productservice.command.api.repository.ProductRepository;
+import com.sun.jdi.event.ExceptionEvent;
+import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@ProcessingGroup("prroduct")
 public class ProductEventHandler {
 
     private final ProductRepository productRepository;
@@ -16,9 +20,15 @@ public class ProductEventHandler {
     }
 
     @EventHandler
-    public void on(ProductCreatedEvent event) {
+    public void on(ProductCreatedEvent event) throws Exception {
         Product product = new Product();
         BeanUtils.copyProperties(event, product);
         productRepository.save(product);
+        throw new Exception("Exception Occurred");
+    }
+
+    @ExceptionHandler
+    public void handle(Exception e) throws Exception {
+        throw e;
     }
 }
