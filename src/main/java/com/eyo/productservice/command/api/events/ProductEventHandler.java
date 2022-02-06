e9a26802d2439badecaaab4944c33d67ca4e18a9
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-02-06 05:55:22 +0000

    :clown_face: Created product entity and repository Implemented a save repository

--- src/main/java/com/eyo/productservice/command/api/events/ProductEventHandler.java
+++ src/main/java/com/eyo/productservice/command/api/events/ProductEventHandler.java
@@ -1,13 +1,24 @@
 package com.eyo.productservice.command.api.events;
 
+import com.eyo.productservice.command.api.data.Product;
+import com.eyo.productservice.command.api.repository.ProductRepository;
 import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductEventHandler {
 
+    private final ProductRepository productRepository;
+
+    public ProductEventHandler(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @EventHandler
     public void on(ProductCreatedEvent event) {
-
+        Product product = new Product();
+        BeanUtils.copyProperties(event, product);
+        productRepository.save(product);
     }
 }
---
+++ src/main/java/com/eyo/productservice/command/api/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.eyo.productservice.command.api.repository;
+
+import com.eyo.productservice.command.api.data.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+}
